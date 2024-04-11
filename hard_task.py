8cd0ab021bad4aba1f617420aaed996c267f89c4
PabloFreitas-lib <pablofreitas.fn@gmail.com>
2024-04-11 13:10:03 +0200

    Hard task description

---
+++ hard_task.py
@@ -0,0 +1,19 @@
+"""
+Maximize Stock Trading Profit
+
+Given the daily values of a stock, create a function called max_profit_days() that, given a list of
+integers, will return the index value of the two elements that represent the day on, which one should
+have bought a share and the day on which one should have sold a share based on the max profit.
+
+A list of integers will represent the stock price at the beginning or "opening bell" of each day for a week. You are required to buy and sell only once. You also must buy a stock before selling it.
+You are required to buy and sell only once. You also musst buy a stock before selling it.
+
+For example, given the list [17, 11, 60, 25, 150, 75, 31, 120] you can assume that index 0 represents 
+day 0 and index 7 represents day 7. In this case, purchasing on day 1 and selling on day 4 would yield 
+the most profit. If we were to call max_profit_days([17, 11, 60, 25, 150, 75, 31, 120), the function 
+would return (1,4).
+This challenge and variations of it were reported to have been asked at interviews with Google. 
+If you've covered the material in Pass the Technical Interview with Python or an equivalent, 
+you i should be able to solve this challenge. If you have trouble, try refreshing your knowledge 
+there first.
+"""
