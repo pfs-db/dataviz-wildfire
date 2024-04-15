21ebd1f89ec7ef75c849f3c1977bc65e1823ff99
PabloFreitas-lib <pablofreitas.fn@gmail.com>
2024-04-15 11:31:34 +0200

    hard task example - Solution - Removed the comments

--- hard_task.py
+++ hard_task.py
@@ -17,3 +17,58 @@ If you've covered the material in Pass the Technical Interview with Python or an
 you i should be able to solve this challenge. If you have trouble, try refreshing your knowledge 
 there first.
 """
+
+import sys
+
+
+def main():
+    sample_list = [17, 11, 60, 25, 150, 75, 31, 120]
+    max_profit_days(sample_list)
+
+
+def max_profit_days(prices: list):
+    """Calculating the prices for every possiblity and afterwards finding the max is would have a O(n!) time, that would be really bad"""
+    if prices == []:
+        return
+    max_profit_days_aux(prices)
+
+
+def max_profit_days_aux(prices: list, max_value=0) -> int:
+    hlist = head(prices)
+    tlist = tail(prices)
+
+    # base case
+    if hlist == [] or tlist == []:
+        return max_value
+    print(f"{hlist} {max(tlist)}")
+    if max_value < max(tlist) - hlist:
+        max_value = max(tlist) - hlist
+    max_profit_days_aux(tlist, max_value)
+
+
+def head(gen_list: list):
+    if gen_list == []:
+        return []
+    else:
+        return gen_list[0]
+
+
+def tail(gen_list: list):
+    if gen_list == []:
+        return []
+    else:
+        return gen_list[1 : len(gen_list)]
+
+
+def map_index(prices: list) -> dict:
+    """[17, 11, 60, 25, 150, 75, 31, 120] -> {"17": 0, "11": 1, ...}"""
+    if prices == []:
+        sys.exit("Price list empty in the map index")
+    price_dict = dict()
+    for price in prices:
+        price_dict[price] = prices.index(price)
+    return price_dict
+
+
+if __name__ == "__main__":
+    main()
